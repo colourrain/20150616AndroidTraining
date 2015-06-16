e83ce2919df602662bc549d813c694fb62451da2
bobo <jiangbo1980@gmail.com>
2015-06-16 17:38:48 +0800

    6.16 commit

--- GitAndroidTest/src/com/example/gitandroidtest/MainActivity.java
+++ GitAndroidTest/src/com/example/gitandroidtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,27 +12,60 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends Activity implements OnClickListener{
+public class MainActivity extends Activity implements OnClickListener {
 
 	EditText edit;
 	Button btn;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		edit=(EditText) findViewById(R.id.edit);
-		btn=(Button) findViewById(R.id.btn_send);
+		edit = (EditText) findViewById(R.id.edit);
+		btn = (Button) findViewById(R.id.btn_send);
 		btn.setOnClickListener(this);
-		
+
 	}
 
 	@Override
 	public void onClick(View v) {
-		Toast.makeText(MainActivity.this, edit.getText().toString(), Toast.LENGTH_SHORT).show();
-		Intent intent=new Intent(MainActivity.this, SecondActivity.class);
+		Toast.makeText(MainActivity.this, edit.getText().toString(),
+				Toast.LENGTH_SHORT).show();
+		Intent intent = new Intent(MainActivity.this, SecondActivity.class);
 		intent.putExtra("Message", edit.getText().toString());
 		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
 		
-		
+		switch (item.getItemId())
+		{
+		case R.id.action_search:
+			Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.action_always:
+			Toast.makeText(this, "Always", Toast.LENGTH_SHORT).show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+
+	}
+
+	@Override
+	public void onOptionsMenuClosed(Menu menu) {
+		// TODO Auto-generated method stub
+		super.onOptionsMenuClosed(menu);
+		Toast.makeText(this, "OptionsMenuClosed", Toast.LENGTH_SHORT).show();
 	}
 }
--- GitAndroidTest/src/com/example/gitandroidtest/SecondActivity.java
+++ GitAndroidTest/src/com/example/gitandroidtest/SecondActivity.java
@@ -1,11 +1,21 @@
 package com.example.gitandroidtest;
 
+import java.util.zip.Inflater;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends Activity {
 	TextView tv;
@@ -18,7 +28,57 @@ public class SecondActivity extends Activity {
 		String str=intent.getStringExtra("Message");
 		tv=(TextView) findViewById(R.id.tv);
 		tv.setText(str);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	    // If your minSdkVersion is 11 or higher, instead use:
+	    // getActionBar().setDisplayHomeAsUpEnabled(true);
 	}
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu items for use in the action bar
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		
+		switch (item.getItemId())
+		{
+		case R.id.action_search:
+			Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+			return true;
+		case R.id.action_always:
+			Toast.makeText(this, "Always", Toast.LENGTH_SHORT).show();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+
+	}
+
+	
+	public void onClick(View view){
+//		Inflater li=(Inflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//		LayoutInflater li=LayoutInflater.from(this);
+//		View view1=li.inflate(R.layout.activity_third, null);
+		View view1=view.inflate(SecondActivity.this, R.layout.activity_third, null);
+
+		setContentView(view1);
+		Button btn3=(Button) view1.findViewById(R.id.btn_3);
+		btn3.setText("Click me");
+		btn3.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(SecondActivity.this, "test", Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+		
+	}
 	
 }
