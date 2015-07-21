59bdd6ec620f3bc9b018bf08f1104f4a91088bbe
bobo <jiangbo1980@gmail.com>
2015-07-21 16:06:16 +0800

    20150721 SVW

--- GitAndroidTest/src/com/example/gitandroidtest/awake/BroadcastReciver_Alarm.java
+++ GitAndroidTest/src/com/example/gitandroidtest/awake/BroadcastReciver_Alarm.java
@@ -1,18 +1,36 @@
 package com.example.gitandroidtest.awake;
 
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 public class BroadcastReciver_Alarm extends BroadcastReceiver{
-
+	static Context a;
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		a=context;
 		Log.i("test", "alarm received");
 		Toast.makeText(context, "alarm", Toast.LENGTH_SHORT).show();
-		
+		AlertDialog.Builder builder=new Builder(context);
+		builder.setTitle("Title").setMessage("Message").setPositiveButton("OK", new OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				Toast.makeText(BroadcastReciver_Alarm.a, "alarm test", Toast.LENGTH_SHORT).show();
+				
+			}
+		});
+		AlertDialog alert = builder.create(); 
+		  alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+		  alert.show();
+
 	}
 	
 
--- GitAndroidTest/src/com/example/gitandroidtest/service/BroadcasterReciver_Service.java
+++ GitAndroidTest/src/com/example/gitandroidtest/service/BroadcasterReciver_Service.java
@@ -13,6 +13,9 @@ public class BroadcasterReciver_Service extends BroadcastReceiver{
 		boolean msg=intent.getBooleanExtra("FINISH", false);
 		Log.i("test","Recevier received " + msg);
 		Toast.makeText(context, "msg=" + msg, Toast.LENGTH_SHORT).show();
+		Intent intent1=new Intent(context, backgroudService.class);
+		intent1.putExtra("service", "Message from service");
+		context.startService(intent1);
 		
 	}
 	
